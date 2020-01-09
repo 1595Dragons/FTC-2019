bf13bf2e929e5aed8032a27c357b698ee52ab176
HenryXII <henryxu1123@gmail.com>
2020-01-09 15:48:59 -0800

    new

--- AutoPickTwoStone7935.java
+++ AutoPickTwoStone7935.java
@@ -6,9 +6,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-@Autonomous(name = "AutoPutStoneAndMoveFoundation7935", group = "official")
+@Autonomous(name = "AutoPickTwoStone7935", group = "official")
 //@Disabled
-public class AutoPutStoneAndMoveFoundation7935 extends LinearOpMode {
+public class AutoPickTwoStone7935 extends LinearOpMode {
 
 
     //basic set up  code for search: 1001
@@ -22,42 +22,69 @@ public class AutoPutStoneAndMoveFoundation7935 extends LinearOpMode {
         robot.ConfigureRobtHardware();
         robot.ConfigureVision();
         double visionY=0,distance;
+        int skyStonePosition=0;
         robot.resetMotorsForAutonomous(robot.left_back, robot.left_front, robot.right_back, robot.right_front);
         robot.status("ready");
         waitForStart();
         robot.targetsSkyStone.activate();
         robot.distinctDrivePlus(DRIVE_SPEED,-29,-29,-29,-29,2.5,-1,0.5);
         visionY=robot.lookForStoneY(0.5);
+        skyStonePosition=1;
         if (visionY==998){
             robot.DriveLeft(SIDE_SPEED,10*robot.team,1);
+            skyStonePosition=0;
             robot.TurnByImu(TURN_SPEED,0,0.5);
             visionY=robot.lookForStoneY(0.5);
             if (visionY==998){
-                robot.DriveLeft(SIDE_SPEED, -21*robot.team,2);
+                skyStonePosition=2;
+                robot.DriveLeft(SIDE_SPEED, -23*robot.team,2);
                 robot.TurnByImu(TURN_SPEED,0,0.5);
             }
         }
 
         //found stone or pick up the last one
         robot.distinctDrivePlus(0.3,10,10,10,10,1.5,1,1.5);
-
         robot.intake_left.setPower(1);
         robot.intake_right.setPower(1);
         robot.DriveForward(DRIVE_SPEED,27,2);
-        //sleep(500);
         robot.intake_left.setPower(0);
         robot.intake_right.setPower(0);
         robot.DriveForward(DRIVE_SPEED,-25,2);
 
 
+
         //travel across field
-        robot.TurnByImu(TURN_SPEED,90*robot.team,1);
-        robot.TurnByImu(TURN_SPEED,90*robot.team,0.5);
-        robot.DriveForward(DRIVE_SPEED,-75,4);
+        robot.TurnByImu(TURN_SPEED,-90*robot.team,1);
+        robot.DriveForward(.7,65-skyStonePosition*8,4);
+        robot.intake_left.setPower(-1);
+        robot.intake_right.setPower(-1);
+        robot.TurnByImu(TURN_SPEED,-90*robot.team,1);
+        robot.DriveForward(.7,-75,4);
+        robot.intake_left.setPower(0);
+        robot.intake_right.setPower(0);
         distance=robot.distance_sensor.getDistance(DistanceUnit.INCH);
-        //telemetry.addData("distance",distance);
-        //telemetry.update();
-        //robot.DriveForward(DRIVE_SPEED,13-distance,1);
+        robot.DriveForward(DRIVE_SPEED,-distance+skyStonePosition*8,1.5);
+        robot.TurnByImu(TURN_SPEED,0,1);
+        //get the stone
+        robot.intake_left.setPower(1);
+        robot.intake_right.setPower(1);
+        robot.DriveForward(DRIVE_SPEED,27,2);
+        robot.intake_left.setPower(0);
+        robot.intake_right.setPower(0);
+        robot.DriveForward(DRIVE_SPEED,-27,2);
+        robot.TurnByImu(TURN_SPEED,-90*robot.team,1);
+        robot.DriveForward(.7,85-skyStonePosition*8,4);
+        robot.intake_left.setPower(-1);
+        robot.intake_right.setPower(-1);
+        robot.DriveForward(.7,-30,4);
+        robot.intake_left.setPower(0);
+        robot.intake_right.setPower(0);
+
+
+
+
+
+        /*
         robot.distinctDrivePlus(DRIVE_SPEED,distance-13,distance-13,distance-13,distance-13,1,-1,0.5);
         robot.TurnByImu(TURN_SPEED,0,1);
 
@@ -83,7 +110,9 @@ public class AutoPutStoneAndMoveFoundation7935 extends LinearOpMode {
         robot.DriveLeft(SIDE_SPEED,35,2.5);
         robot.TurnByImu(TURN_SPEED,90*robot.team,1);
         robot.distinctDrivePlus(0.6,-25,-25,-25,-25,1.5,1,1);
-        //robot.distinctDrivePlus(0.7,25,-25,-25,25,2,1,1);
+        */
+
+
         robot.targetsSkyStone.deactivate();
     }
 
